d55c81bc0071c8b6d9c551286b3bc1149acc9a54
natha <natha@192.168.56.1>
2017-03-31 20:40:02 -0500

    Exam 2 Completed Project

---
+++ src/FunctionDemo.cpp
@@ -0,0 +1,69 @@
+//==============================================================================
+// Exam 2 - Function Demo
+//==============================================================================
+/*
+    File: FunctionDemo.cpp
+    Project: Exam 2
+    Author: Nathaniel Hoefer
+    Student ID: X529U639
+    Class: CS411 - Spring 2017
+	Date: 3/31/2017
+
+	This program tests the given values to determine whether they meet the
+	requirements of either only containing letters or only containing numbers.
+
+******************************************************************************/
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include "Function1.h"
+#include "Function2.h"
+
+using namespace std;
+
+int main()
+{
+	// Given strings to test
+	vector<string> input;
+	input.push_back("123456");
+	input.push_back("ab12AB");
+	input.push_back("ab12A*");
+	input.push_back("abCDef");
+	input.push_back("ab12AB");
+	input.push_back("ab12A*");
+
+	// Number of loops per numbers/letters
+	int digitStrings = input.size() / 2;
+	int alphaStrings = input.size() - digitStrings - 1;
+
+	// Run digit tests
+	for (int i = 0; i < digitStrings; i++) {
+		cout << "digit : isValid(\"" + input[i] + "\"): ";
+		try {
+			if(digitVerification::isValid(input[i])) {
+				cout << "true" << endl;
+			} else {
+				cout << "false" << endl;
+			}
+		} catch (invalid_argument &e) {
+			cout << e.what() << endl;
+		}
+	}
+
+	// Run letter tests
+	for (int i = alphaStrings; i < (int) input.size(); i++) {
+		cout << "alpha : isValid(\"" + input[i] + "\"): ";
+		try {
+			if(alphaVerification::isValid(input[i])) {
+				cout << "true" << endl;
+			} else {
+				cout << "false" << endl;
+			}
+		} catch (invalid_argument &e) {
+			cout << e.what() << endl;
+		}
+	}
+}
+
+
